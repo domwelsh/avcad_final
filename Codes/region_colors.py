3eca2c647a6f108eb79d426a5737b4e8f7258eb7
Dominic Welsh <domwelsh@gmail.com>
2024-06-20 19:04:54 +0100

    Adding region_color.py

---
+++ Codes/region_colors.py
@@ -0,0 +1,9 @@
+region_colors = {
+    'Alentejo': '#FF69B4',  # pink
+    'Norte': '#32CD32',  # green
+    'Centro': '#6495ED',  # blue
+    'Açores': '#FFD700',  # yellow
+    'Algarve': '#8B0A1A',  # red
+    'Lisboa': '#4682B4',  # light blue
+    'Madeira': '#9ACD32'  # light green
+}
