9ad50abd761f709fc7ce02766e001342cb821367
Dominic Welsh <domwelsh@gmail.com>
2024-06-21 18:24:46 +0100

    Updates

---
+++ Codes/production_colors.py
@@ -0,0 +1,18 @@
+production_colors = {
+    'Citrus plantations':'#1f77b4',
+    'Fresh fruit plantations (excluding citrus plantations)': '#ff7f0e',
+    'Fruit plantations (subtropical climate zones)': '#2ca02c',
+    'Nuts plantations': '#d62728',
+    'Olive plantations': '#9467bd',
+    'Other permanent crops': '#8dd3c7',
+    'Vineyards': '#fdb462',
+    'Cereals': '#bcbd22',
+    'Dried pulses': '#17becf',
+    'Flowers and ornamental plants': '#e377c2',
+    'Fodder plants': '#7f7f7f',
+    'Fresh vegetables': '#bcbd22',
+    'Industrial crops': '#ff9896',
+    'Other temporary crops': '#aec7e8',
+    'Potatoes': '#9edae5',
+    'Temporary grasses and grazings': '#ffbb78'
+}
